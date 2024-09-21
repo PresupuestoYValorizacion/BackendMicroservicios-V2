54fd3d54d4c4c1e8947392f87658bca329d61d4e
Marvin Jara <joelmarvinjara@gmail.com>
2024-09-20 23:23:50 -0500

    feat: Se realizo una nueva migracion
    Se modifico la clase usuario_licencias

---
+++ src/ms-acceso/MsAcceso.Infrastructure/Migrations/EnterpriseDb/20240920162525_initialApp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MsAcceso.Infrastructure.Migrations.EnterpriseDb
+{
+    /// <inheritdoc />
+    public partial class initialApp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "presupuestos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_presupuestos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user-tenant",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user-tenant", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user-tenant_Email",
+                table: "user-tenant",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "presupuestos");
+
+            migrationBuilder.DropTable(
+                name: "user-tenant");
+        }
+    }
+}
